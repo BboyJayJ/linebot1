684aea2ede7e8ae0903dcea9d7e64c27bfeb72a9
BboyJayJ <fbiko888@gmail.com>
2023-02-14 21:21:50 +0800

    優化程式碼

--- bot/views.py
+++ bot/views.py
@@ -7,11 +7,14 @@ from django.views.decorators.csrf import csrf_exempt
 
 from linebot import LineBotApi, WebhookHandler,WebhookParser
 from linebot.exceptions import InvalidSignatureError,LineBotApiError
-from linebot.models import MessageEvent,TextSendMessage,TextMessage
+from linebot.models import MessageEvent,TextSendMessage,TextMessage,ImageSendMessage
 import random
 
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parse=WebhookParser(settings.LINE_CHANNEL_SECRET)
+
+def index(request):
+    return HttpResponse("<h1> LineBot APP </h1>")
 
 @csrf_exempt    
 def callback(request):
@@ -27,6 +30,7 @@ def callback(request):
         for event in events:
             if isinstance(event,MessageEvent):
                 if isinstance(event.message,TextMessage):
+                    message,img_url=None, None
                     text=event.message.text
                     print(text)
 
@@ -34,30 +38,28 @@ def callback(request):
                     if '台北捷運' in text:
                         message='https://www.metro.taipei/cp.aspx?n=91974F2B13D997F1'
 
-
+                    elif '台中捷運' in text:
+                        img_url='https://assets.piliapp.com/s3pxy/mrt_taiwan/taichung/20201112_zh.png?v=2'                       
                     elif '電影' in text:
                         message='https://movies.yahoo.com.tw/movie_intheaters.html'
-
                     elif '樂透' in text:
                         message=lotto()
-
                     elif '早安' in text:
-                        message='早安你好'
-                        
+                        message='早安你好'                       
                     else:
                          message=random.choice(words)
                 
-                    line_bot_api.reply_message(
-                        event.reply_token,
-                        
-                        TextSendMessage(text=message)
-                    )
-                else:
-                    line_bot_api.reply_message(
-                        event.reply_token,
-                        
-                        TextSendMessage(text='無法解析')
-                    )
+                                        
+                else:  
+                    message='無法解析'
+
+                messageObject = TextSendMessage(text=message) if message is not None else \
+                        ImageSendMessage(original_content_url=img_url,
+                            preview_image_url=img_url)
+                    
+                line_bot_api.reply_message(event.reply_token, messageObject)
+
+
 
         return HttpResponse()
     else:
